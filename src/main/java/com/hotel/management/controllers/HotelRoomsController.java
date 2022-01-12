2f11c783df48e38e1fa571cc087408c25a00ea62
ajith <ajithpudari@gmail.com>
2022-01-12 17:03:51 +0530

    check

--- src/main/java/com/hotel/management/controllers/HotelRoomsController.java
+++ src/main/java/com/hotel/management/controllers/HotelRoomsController.java
@@ -1,5 +1,8 @@
 package com.hotel.management.controllers;
 
-public class HotelRoomsController {
-
+public class HotelRoomsController{
+    public String display(){
+        System.out.println("hi");
+        return "hi";
+    }
 }
